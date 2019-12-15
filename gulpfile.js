22f2cae2e123f13d8c5af6160a2cd4f2aa1f6f92
thelittlestschnauzer <doneil1580@gmail.com>
2019-12-15 00:24:27 -0500

    starter-file

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//  compile scss into css
+function style() {
+  //  where is my scss file
+  return gulp.src('./scss/**/*.scss')
+  //  pass that file through sass compiler
+    .pipe(sass().on('error', sass.logError))
+  //  where do I save the compiled css
+    .pipe(gulp.dest('./css'))
+    //  stream changes to all browser
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
+
